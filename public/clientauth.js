9b79595c1c7cd19622739e1b5d8ed5efaf6d70b0
Matthew Sterrett <sterretm@oregonstate.edu>
2019-05-22 20:11:26 -0700

    Set up some filestructure and created some function templates

---
+++ public/clientauth.js
@@ -0,0 +1,29 @@
+
+// Takes a username and a password, then logs the user in
+// (creates valid user authentication token)
+// Args: username, password, callback
+// Returns: calls callback with (userID) or (undefined)
+// if login fails
+function loginUser(username, password, callback){
+	
+	callback();
+}
+
+// Takes a userID and logs the user out
+// This invalidates there login token
+// Args: userID, callback
+// Returns: calls callback() with no argument
+function logoutUser(userID, callback){
+	
+	callback();
+}
+
+// Takes a username and password, then creates an account
+// using those credentials, in addition to logging them in
+// Args: username, password
+// Returns: calls callback with (userID) or (undefined) if 
+// signup fails
+function signupUser(username, password){
+	
+	callback();
+}
---
+++ scripts/auth.js
@@ -0,0 +1,6 @@
+
+// Takes a 
+
+function validateUser(req, callback){
+	
+}
